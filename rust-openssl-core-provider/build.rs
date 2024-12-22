ad65388899a9a47f19a17b784d7356795de28e9d
Nicola Tuveri <nic.tuv@gmail.com>
2024-12-22 21:31:51 +0200

    feat: add pkg-config dependency and update build.rs
    - Added pkg-config as a dependency in Cargo.lock and Cargo.toml.
    - Updated flake.lock and flake.nix to include pkg-config.
    - Modified build.rs to use pkg-config for OpenSSL library probing.
    - Adjusted nativeBuildInputs and dockerImage-runner configurations.

--- rust-openssl-core-provider/build.rs
+++ rust-openssl-core-provider/build.rs
@@ -1,21 +1,24 @@
 use std::env;
 use std::path::PathBuf;
 
-fn main() {
-    // Tell cargo to look for shared libraries in the specified directory
-    //println!("cargo:rustc-link-search=/path/to/lib");
-
-    // Tell cargo to tell rustc to link the system bzip2
-    // shared library.
-    //println!("cargo:rustc-link-lib=bz2");
-
+fn generate_bindings() {
     // Tell cargo to invalidate the built crate whenever the wrapper changes
     println!("cargo:rerun-if-changed=include/wrapper.h");
+
+    // This might require to correctly setup the PKG_CONFIG_PATH env variable
+    // e.g., export PKG_CONFIG_PATH="<my_custom_ossl_path>/lib/pkgconfig:$PKG_CONFIG_PATH"
+    let openssl = pkg_config::probe_library("openssl").unwrap();
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
+        .clang_args(
+            openssl
+                .include_paths
+                .iter()
+                .map(|path| format!("-isystem{}", path.to_string_lossy())),
+        )
         // The input header we would like to generate
         // bindings for.
         .header("include/wrapper.h")
@@ -34,4 +37,15 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
+}
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+    //println!("cargo:rustc-link-search=/path/to/lib");
+
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    //println!("cargo:rustc-link-lib=bz2");
+
+    generate_bindings()
 }
