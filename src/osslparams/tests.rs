893dfd52016cea75b2810523ed5bf931afdaa902
Nicola Tuveri <nic.tuv@gmail.com>
2025-03-15 14:47:27 +0200

    tests(osslparams): use log::trace instead of log::debug
    Updated log statements in tests.rs from debug to trace level for better 
    granularity in logging. This change ensures that detailed logs are 
    available without cluttering the debug level output.

--- src/osslparams/tests.rs
+++ src/osslparams/tests.rs
@@ -27,27 +27,27 @@ mod generic {
             key: ptr::null(),
         };
         let result = IntData::try_from(&mut ossl_param as *mut OSSL_PARAM);
-        log::debug!("IntData::try_from returned: {result:?}");
+        log::trace!("IntData::try_from returned: {result:?}");
         // Check that the result is Ok and properly returns IntData
         assert!(result.is_ok());
 
         ossl_param.data_type = 0;
         let result = IntData::try_from(&mut ossl_param as *mut OSSL_PARAM);
-        log::debug!("(expected an error) {result:?}");
+        log::trace!("(expected an error) {result:?}");
         assert!(result.is_err());
 
         let k = c"test_key";
         let op_utf8str = OSSLParam::new_const_utf8string(k, Some(c"test_value"));
         let t: *const OSSL_PARAM = std::ptr::from_ref(&op_utf8str);
         let result = Utf8StringData::try_from(t as *mut OSSL_PARAM);
-        log::debug!("{result:?}");
+        log::trace!("{result:?}");
         // Check that the result is Ok
         assert!(result.is_ok());
 
         let op = OSSLParam::try_from(t as *mut OSSL_PARAM);
         assert!(op.is_ok());
         let op = op.unwrap();
-        log::debug!("{op:?}");
+        log::trace!("{op:?}");
         assert_eq!(op.get_data_type().unwrap(), OSSL_PARAM_UTF8_STRING);
         assert_eq!(op.get_key().unwrap(), k);
         assert_eq!(op.get::<&CStr>(), Some(c"test_value"));
