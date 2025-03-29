d34f42468907e8dd832e869a3e74b6a0f025e982
Nicola Tuveri <nic.tuv@gmail.com>
2025-03-29 12:10:39 +0200

    refactor(operations/keymgmt): modularize keymgmt operations
    Move the `keymgmt` module under a new dedicate `operations` module,
    for better organization, maintainability, and discoverability.
    
    Changes:
    - Added `src/operations.rs` and `src/operations/keymgmt.rs`.
    - Removed `keymgmt` module from `src/lib.rs`.
    
    This improves code structure and aligns with modular design principles.

--- src/lib.rs
+++ src/lib.rs
@@ -5,48 +5,11 @@
 
 pub mod bindings;
 pub mod capabilities;
+pub mod operations;
 pub mod ossl_callback;
 pub mod osslparams;
 
 pub type OurError = anyhow::Error;
-
-pub mod keymgmt {
-    pub mod selection {
-        use super::super::bindings;
-        use anyhow::anyhow;
-        use bitflags::bitflags;
-        use std::fmt::Debug;
-        use std::result::Result::Ok;
-
-        bitflags! {
-        #[derive(Debug)]
-                        pub struct Selection: u32 {
-            const PRIVATE_KEY = bindings::OSSL_KEYMGMT_SELECT_PRIVATE_KEY;
-            const PUBLIC_KEY = bindings::OSSL_KEYMGMT_SELECT_PUBLIC_KEY;
-            const DOMAIN_PARAMETERS = bindings::OSSL_KEYMGMT_SELECT_DOMAIN_PARAMETERS;
-            const OTHER_PARAMETERS = bindings::OSSL_KEYMGMT_SELECT_OTHER_PARAMETERS;
-
-            const ALL_PARAMETERS = bindings::OSSL_KEYMGMT_SELECT_ALL_PARAMETERS;
-            const KEYPAIR = bindings::OSSL_KEYMGMT_SELECT_KEYPAIR;
-            const ALL = bindings::OSSL_KEYMGMT_SELECT_ALL;
-                        }
-                    }
-
-        impl TryFrom<u32> for Selection {
-            type Error = crate::OurError;
-
-            fn try_from(value: u32) -> Result<Self, Self::Error> {
-                match Selection::from_bits(value) {
-                    Some(s) => Ok(s),
-                    None => Err(anyhow!(
-                        "Invalid OSSL_KEYMGMT_SELECT flag value: {:?}",
-                        value
-                    )),
-                }
-            }
-        }
-    }
-}
 
 use num_enum::{Default, IntoPrimitive, TryFromPrimitive};
 
---
+++ src/operations/keymgmt.rs
@@ -0,0 +1,34 @@
+pub mod selection {
+    use crate::bindings;
+    use bitflags::bitflags;
+    use std::fmt::Debug;
+    use std::result::Result::Ok;
+
+    bitflags! {
+        #[derive(Debug)]
+        pub struct Selection: u32 {
+            const PRIVATE_KEY = bindings::OSSL_KEYMGMT_SELECT_PRIVATE_KEY;
+            const PUBLIC_KEY = bindings::OSSL_KEYMGMT_SELECT_PUBLIC_KEY;
+            const DOMAIN_PARAMETERS = bindings::OSSL_KEYMGMT_SELECT_DOMAIN_PARAMETERS;
+            const OTHER_PARAMETERS = bindings::OSSL_KEYMGMT_SELECT_OTHER_PARAMETERS;
+
+            const ALL_PARAMETERS = bindings::OSSL_KEYMGMT_SELECT_ALL_PARAMETERS;
+            const KEYPAIR = bindings::OSSL_KEYMGMT_SELECT_KEYPAIR;
+            const ALL = bindings::OSSL_KEYMGMT_SELECT_ALL;
+        }
+    }
+
+    impl TryFrom<u32> for Selection {
+        type Error = crate::OurError;
+
+        fn try_from(value: u32) -> Result<Self, Self::Error> {
+            match Selection::from_bits(value) {
+                Some(s) => Ok(s),
+                None => Err(anyhow::anyhow!(
+                    "Invalid OSSL_KEYMGMT_SELECT flag value: {:?}",
+                    value
+                )),
+            }
+        }
+    }
+}
